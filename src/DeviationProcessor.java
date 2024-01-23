9bd1706e1b185e431f1d4a3aa271ecb7b6819554
Vadim Podogov <VPodogov@croc.ru>
2024-01-23 10:58:16 +0300

    logic errors fixed

--- src/DeviationProcessor.java
+++ src/DeviationProcessor.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,23 +18,34 @@ public class DeviationProcessor {
 
     public Map<LocalDate, Set<String>> findDateToDeviatedPersons(List<TeaBrewing> teaBrewingList) {
         var dateToEmployeeNames = new HashMap<LocalDate, Set<String>>();
+        var dateToIncorrectBrewingEmployees = new HashMap<LocalDate, Set<String>>();
         teaBrewingList.forEach(teaBrewing -> {
+            LocalDate brewingDate = teaBrewing.timeTo().toLocalDate();
+            String brewingEmployee = teaBrewing.employeeName();
             if (isBrewingCorrect(teaBrewing)) {
-                dateToEmployeeNames.computeIfAbsent(teaBrewing.timeTo().toLocalDate(),
-                    k -> new HashSet<>(Set.of(teaBrewing.employeeName())));
+                if (!dateToIncorrectBrewingEmployees
+                    .getOrDefault(brewingDate, Collections.emptySet())
+                    .contains(brewingEmployee)) {
+                    dateToEmployeeNames
+                        .computeIfAbsent(brewingDate, k -> new HashSet<>())
+                        .add(brewingEmployee);
+                }
             } else {
-                dateToEmployeeNames.computeIfPresent(teaBrewing.timeTo().toLocalDate(), (k, v) -> {
-                    v.remove(teaBrewing.employeeName());
+                dateToEmployeeNames.computeIfPresent(brewingDate, (k, v) -> {
+                    v.remove(brewingEmployee);
                     return v;
                 });
+                dateToIncorrectBrewingEmployees
+                    .computeIfAbsent(brewingDate, k -> new HashSet<>())
+                    .add(brewingEmployee);
             }
         });
         return dateToEmployeeNames;
     }
 
     private boolean isBrewingCorrect(TeaBrewing brewing) {
-        return brewing.timeFrom().toLocalTime().isAfter(LocalTime.of(8, 0)) &&
-            brewing.timeFrom().toLocalTime().isBefore(LocalTime.of(23, 0)) &&
+        return !brewing.timeFrom().toLocalTime().isBefore(LocalTime.of(8, 0, 0)) &&
+            !brewing.timeFrom().toLocalTime().isAfter(LocalTime.of(23, 0, 0)) &&
             brewing.brewingTime() >= brewing.tea().teaType().timeFrom() &&
             brewing.brewingTime() <= brewing.tea().teaType().timeTo() &&
             brewing.temperature() >= brewing.tea().teaType().temperatureFrom() &&
